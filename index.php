808bc5145be756d91e4dc4399d2604e05bf89a85
itcomputer <info@itcomputer.fr>
2013-11-19 21:28:02 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Serveur téléphonique ITCPBX V1</title>
+<style type="text/css">
+.style {
+        font-weight: bold;
+        color: #000;
+}
+body
+{
+        background-color: #046380;
+        text-align: center;
+        color: #09A4FE;
+}
+</style>
+</head>
+
+<body>
+<img src="itcpbx_site.png">
+</body>
+</html>
